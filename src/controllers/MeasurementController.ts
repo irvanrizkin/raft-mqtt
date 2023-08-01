fcc7a175bee569c20171bca025e5516fa89219a0
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-08-01 14:36:05 +0700

    feat: get sampled 1d or 1w measurements

--- src/controllers/MeasurementController.ts
+++ src/controllers/MeasurementController.ts
@@ -9,8 +9,23 @@ export class MeasurementController extends Controller {
 
   listByPeriod = async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
+    const { period } = req.query;
 
     try {
+      if (period) {
+        const procedure = this.getProcedureFromPeriod(period.toString());
+
+        const { data, error } = await this.listAverageByPeriod(id, procedure);
+
+        if (error) throw new Error(error.message);
+
+        return res.status(200).json({
+          status: true,
+          message: 'measurement sampled ${period} listed successfully',
+          results: data,
+        });
+      }
+
       const { data, error } = await this.listLatest30Min(id);
 
       if (error) throw new Error(error.message);
@@ -31,5 +46,24 @@ export class MeasurementController extends Controller {
       .eq('deviceId', deviceId)
       .gte('createdAt', sub(new Date(), { minutes: 30 }).toISOString())
       .order('createdAt');
+  }
+
+  private listAverageByPeriod = async (
+    deviceId: string,
+    procedure: "get_daily_sample" | "get_weekly_sample",
+  ) => {
+    return await this.supabase.rpc(procedure, {
+      p_deviceid: deviceId,
+    });
+  }
+
+  private getProcedureFromPeriod(period: string) {
+    if (period === '1d') {
+      return "get_daily_sample";
+    }
+    if (period === '1w') {
+      return "get_weekly_sample";
+    }
+    throw new Error("Invalid period value. Only '1d' or '1w' are supported.");
   }
 }
